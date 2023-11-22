8cfee68d4c625fa17881aac250090e187c816534
SofiaaO <sofiaortizh1@gamil.com>
2023-11-22 18:30:35 -0400

    avance maquetacion

---
+++ script.js
@@ -0,0 +1,77 @@
+let tipoNumero= 7.438043
+let tipoCadenaDeTexto= 'Hola Mundo'
+let tipoBooleano= false
+let tipoUndefinido= undefined
+let tipoNulo= null
+
+let tipoObjeto= {
+    name: "Sofia",
+    password: "9292",
+    adress:{
+        country: "Venezuela",
+        city: "Valencia"
+    }
+}
+
+let tipoLista=[  
+    tipoObjeto,
+    tipoObjeto,
+    tipoObjeto,
+    tipoObjeto,
+]
+/*
+if (2>6) {
+    console.log("Se cumplió la condicion")
+} 
+else {
+    console.log("No se cumplió la condición")
+}
+
+/* Operador Ternario resume el IF 
+(2>6)
+? console.log("Se cumplió la condicion")
+: console.log("No se cumplió la condición") */
+
+
+
+
+let MifuncionFlecha=
+(num1, num2) => {
+    
+    return num1 + num2
+}
+
+console.log(MifuncionFlecha(5,8))
+
+
+ 
+/**
+ * OPERADORES
+ * 
+ * =         asignación
+ * 
+ * 
+ * Operadores Lógicos
+ * 
+ * ==        igual que
+ * !=        distinto de
+ * >=        mayor o igual que
+ * <=        menor o igual que
+ * >         mayor que 
+ * <         menor que
+ * 
+ * Operadores Matemáticos
+ * 
+ * +         sumar
+ * -         restar
+ * /         dividir
+ * *         multiplicar
+ * %         ?
+ * 
+ * 
+ * Operadores
+ * 
+ * &&         and    y
+ * ||         or     o
+ * 
+ */
